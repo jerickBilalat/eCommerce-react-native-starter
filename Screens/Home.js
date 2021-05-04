1f7ecac1698bb147b818cfb5df4a795dd64cc69e
Jerick Bilalat <jrk.email@yahoo.com>
2021-05-04 12:56:45 -0500

    edit homepage content

--- Screens/Home.js
+++ Screens/Home.js
@@ -5,8 +5,7 @@ import { View, Button } from "react-native";
 export default function Home({ navigation }) {
   return (
     <View>
-      <Text>Home is here</Text>
-      <Button onPress={() => navigation.navigate("Pdp")} title="Go to Pdp" />
+      <Text>Home Page</Text>
     </View>
   );
 }
